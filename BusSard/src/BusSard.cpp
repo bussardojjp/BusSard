0c1bfa3ffd4e0d9554f9401752784dc99b50e99b
90788 <90788@NBGR549PC06.edu01.education-siemens.net>
2018-02-09 10:43:20 +0100

    merge

--- BusSard/src/BusSard.cpp
+++ BusSard/src/BusSard.cpp
@@ -6,17 +6,16 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-<<<<<<< HEAD
+
 #include <nana/gui/wvl.hpp>
 #include <nana/gui/widgets/label.hpp>
-#include "gui/GUIBuilder.h";
-=======
+#include "gui/GUIBuilder.h"
+
 #include <parser/ConfigurationCreator.h>
->>>>>>> branch 'master' of https://github.com/bussardojjp/BusSard
 
 int main()
 {
-<<<<<<< HEAD
+
 	using namespace nana;
 	GUI::GUIBuilder guiBuilder;
 	guiBuilder.buildGUI();
@@ -25,8 +24,8 @@ int main()
     lb.caption("Hello, World");
     fm.show();
     exec();*/
-=======
+
    Parser::ConfigurationCreator cf("resource/modules/test.xml");
    cf.readConfig();
->>>>>>> branch 'master' of https://github.com/bussardojjp/BusSard
+
 }
