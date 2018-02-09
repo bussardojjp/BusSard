673b39711716bdb1b9045935bb525cba59c59917
90788 <90788@NBGR549PC06.edu01.education-siemens.net>
2018-02-09 08:11:40 +0100

    test ordner

---
+++ BusSard/include/test/GUItest.h
@@ -0,0 +1,17 @@
+/*
+ * GUItest.h
+ *
+ *  Created on: 09.02.2018
+ *      Author: 90788
+ */
+
+#ifndef TEST_GUITEST_H_
+#define TEST_GUITEST_H_
+
+class GUItest {
+public:
+	GUItest();
+	virtual ~GUItest();
+};
+
+#endif /* TEST_GUITEST_H_ */
---
+++ BusSard/src/test/GUItest.cpp
@@ -0,0 +1,18 @@
+/*
+ * GUItest.cpp
+ *
+ *  Created on: 09.02.2018
+ *      Author: 90788
+ */
+
+#include "test/GUItest.h"
+
+GUItest::GUItest() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GUItest::~GUItest() {
+	// TODO Auto-generated destructor stub
+}
+
